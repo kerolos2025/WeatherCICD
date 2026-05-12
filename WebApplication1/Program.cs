46f93145d62bc32d715f50a32be43f9fe49b13bd
kerolos <kero.gpt@gmail.com>
2026-05-12 19:23:00 +0300

    modify program.cs for swagger

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -10,12 +10,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
